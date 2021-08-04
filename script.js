76b5f42f3b17dbf6ad211f2bc40a8f68fd5d9fe7
nugi-developers <68599801+Erehmaryann@users.noreply.github.com>
2021-08-04 15:56:32 -0700

    finished fetching data from an Api with JavaScript

---
+++ script.js
@@ -0,0 +1,24 @@
+// Global variable "apiQuotes"
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  //Pick a random quote from apiQuotes array
+  const randomQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(randomQuote);
+}
+
+// Get Quotes fron API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const res = await fetch(apiUrl);
+    apiQuotes = await res.json();
+    newQuote();
+  } catch (e) {
+    //Catch error here
+    // console.log(e, "Error getting quotes");
+  }
+}
+//callin the func on load
+getQuotes();
